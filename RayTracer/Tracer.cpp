ab661869c09694517c4d1e5b3e837e9dd9b9edf5
Ghostninja2451 <80051664+Ghostninja2451@users.noreply.github.com>
2021-11-04 19:21:38 -0600

    11/04/21

--- RayTracer/Tracer.cpp
+++ RayTracer/Tracer.cpp
@@ -1,9 +1,11 @@
 #include "Tracer.h"
 #include"Scene.h"
+#include<iostream>
 
 void Tracer::Trace(const ColorBuffer& colorBuffer, Scene* scene, Camera* camera)
 {
 	float aspectRatio = colorBuffer.width / (float)colorBuffer.height;
+	float invSample = 1.0f / samples;
 
 	for (int y = 0; y < colorBuffer.height; y++)
 	{
@@ -13,28 +15,22 @@ void Tracer::Trace(const ColorBuffer& colorBuffer, Scene* scene, Camera* camera)
 
 			for (int sample = 0; sample < samples; sample++)
 			{
-
-				//glm::vec2 point;
-				//point.x = (x + random01()) / (float)colorBuffer.width;
-				//point.y = 1 - ((y + random01()) / (float)colorBuffer.height);
-			
-				//glm::vec3 direction = glm::vec3{ (point * 2.0f) - 1.0f, -1 };
-				//direction.y /= aspectRatio;
-				//direction = glm::normalize(direction);
-
-				//ray_t ray{ {0, 0, 0}, direction };
-
 				glm::vec2 viewport = camera->ScreenToViewport({ x + random01(), y + random01() });
 				viewport.y = 1 - (viewport.y);
 				ray_t ray = camera->ViewportToRay(viewport);
 
 				raycastHit_t hit;
-				color += scene->Trace(ray, 0.001f, FLT_MAX, hit);
+				color += scene->Trace(ray, 0.001f, FLT_MAX, hit, depth);
 			}
 
-			color /= (float)samples;
+			//color /= (float)samples;
+
+			color.r = sqrt(color.r * invSample);
+			color.g = sqrt(color.g * invSample);
+			color.b = sqrt(color.b * invSample);
 
 			colorBuffer.SetColor(x, y, Vec3ToColor(color));
 		}
+		std::cout << y << std::endl;
 	}
 }
